669d42a16a996f6e9f46c634ae37690a1c7ef3be
d-a-tacito <dananto02@gmail.com>
2022-12-13 00:03:51 +0300

    Added MoviesApp with mappers, middlewares

---
+++ Middleware/ActorLogMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Logging;
+using MoviesApp.Controllers;
+using MoviesApp.Data;
+using MoviesApp.Middleware;
+using MoviesApp.Models;
+using MoviesApp.ViewModels;
+
+namespace MoviesApp.Middleware
+{
+    public class ActorLogMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ActorLogMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext, ILogger<ActorLogMiddleware> logger)
+        {
+            if (httpContext.Request.Path.Equals("/Actors"))
+            {
+                logger.LogInformation("OPENED ACTORS' LIST\n"+$"Request: {httpContext.Request.Path}  Method: {httpContext.Request.Method}");
+            }
+            else if (httpContext.Request.Path.Equals("/Actors/Index"))
+            {
+                logger.LogInformation("OPENED ACTORS' LIST INDEX\n"+$"Request: {httpContext.Request.Path}  Method: {httpContext.Request.Method}");
+            }
+            await _next(httpContext);
+        }
+    }
+}
---
+++ ViewModels/InputActorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using MoviesApp.Models;
+
+namespace MoviesApp.ViewModels;
+
+public class InputActorViewModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    
+    [DataType(DataType.Date)]
+    public DateTime BirthDate { get; set; }
+}
